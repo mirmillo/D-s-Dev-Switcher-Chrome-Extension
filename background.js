bc33485437bfa039980a5b0bb590d49ee0ae5c03
David <mirmillo@gmail.com>
2012-09-04 16:23:44 -0400

    Removed redundant code

--- background.js
+++ background.js
@@ -6,21 +6,15 @@ function updateUrl(tab){
   if(tab.url.match(local))
   {
       var newurl = tab.url.replace(local, remote);
-      chrome.tabs.update(tab.id, {url: newurl});
   } 
   else if(tab.url.match(dubs))
   {
       var newurl = tab.url.replace(dubs, local);
-      chrome.tabs.update(tab.id, {url: newurl});
   } 
   else {
-      //var newurl = tab.url.replace(remote, local);
-	  //var taburl = encodeURIComponent(tab.url);
-	  //var newurl =  local + tab.url; 
       var newurl = tab.url.replace(remote, local);
-	  //console.log( newurl );
-      chrome.tabs.update(tab.id, {url: newurl});
   }
+  chrome.tabs.update(tab.id, {url: newurl});
 }
 
 chrome.browserAction.onClicked.addListener(function(tab) {updateUrl(tab);});
